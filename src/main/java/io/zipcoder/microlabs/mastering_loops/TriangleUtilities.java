8acc7661408f5b3ce1521051c695764b922d9dd2
tb0902 <tbruton417@gmail.com>
2024-03-11 01:29:47 -0400

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -15,16 +15,38 @@ public class TriangleUtilities {
         for (int i = 0; i < numberOfStars; i++) {
             rows.append("*");
         }
-            return rows.toString();
-        }
-
-
-        public static String getSmallTriangle () {
-            return null;
-        }
-
-
-        public static String getLargeTriangle () {
-            return null;
-        }
+        return rows.toString();
     }
+
+
+    public static String getSmallTriangle() {
+        int numberOfRows = 5;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < numberOfRows; i++) {
+            for (int j = 1; j < numberOfRows; j++) {
+                if (i >= j) {
+                    sb.append("*");
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+
+    public static String getLargeTriangle() {
+        int numberOfRows = 10;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < numberOfRows; i++) {
+            for (int j = 1; j < numberOfRows; j++) {
+                if (i >= j) { // when there's more lines than stars, append a star
+                    sb.append("*"); // j will add a star for each iteration of i
+                }
+            }
+            sb.append("\n"); // i will add a new line for each row
+        }
+        return sb.toString();
+    }
+}
